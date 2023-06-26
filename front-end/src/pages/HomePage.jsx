0f0718a2ac32c47092c901270b2e74404a4315cd
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-06-25 22:53:20 -0300

    feat: best deals and productcard

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Layout/Header";
 import Hero from "../components/Layout/Hero";
 import Categories from "../components/Layout/Categories";
+import BestDeals from "../components/Layout/BestDeals";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Header activeHeading={1} />
       <Hero />
       <Categories />
+      <BestDeals />
     </div>
   );
 };
