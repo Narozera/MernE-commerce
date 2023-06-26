8b00ce771600f17e0dc3a56fab09aeab7b35022d
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-06-25 22:19:48 -0300

    feat: categories component

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Layout/Header";
 import Hero from "../components/Layout/Hero";
+import Categories from "../components/Layout/Categories";
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 };
